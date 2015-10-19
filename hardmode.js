f60d8e6ce9f0c28741d17261a884f9a423dc09ad
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-19 09:17:49 -0400

    cleaned up and added comments

--- hardmode.js
+++ hardmode.js
@@ -1,39 +1,22 @@
-var clearBtn = document.getElementById("clear");
-var equalsBtn = document.getElementById("equals");
+//set variable for answer box
 var answerBox = document.getElementById("answer-box");
-var calculator = document.getElementById("calculator");
 
-function addText(button) {
+//add corresponding text for the button that was clicked to the answer box
+function addText(button){
   var text;
-  switch (button.id){
-    case "add":
-      text = " + ";
-      break;
-    case "subtract":
-      text = " - ";
-      break;
-    case "multiply":
-      text = " * ";
-      break;
-    case "divide":
-      text = " / ";
-      break;
-    default:
-      text = button.innerHTML;
-      break;
-      }
+  if (button.id==="multiply"){
+    text=" * "
+  }else if (button.classList.contains("operator")) {
+    text = " "+ button.innerHTML +" ";
+  }
+  else{
+    text = button.innerHTML;
+  }
   answerBox.innerHTML = answerBox.innerHTML + text;
 }
 
-function clearText() {
-  answerBox.innerHTML = "";
-}
-
-function evaluateExpression(){
-  answerBox.innerHTML = eval(answerBox.innerHTML);
-}
-
-calculator.addEventListener("click", function(e) {
+//if a button in the calculator that has the text class is clicked, call addtext
+document.getElementById("calculator").addEventListener("click", function(e) {
   var classes = e.target.className.split(" ");
   if (classes){
     classes.forEach(function(item) {
@@ -44,5 +27,12 @@ calculator.addEventListener("click", function(e) {
   }
 });
 
-clearBtn.addEventListener("click",clearText);
-equalsBtn.addEventListener("click",evaluateExpression);
+//if the clear button is clicked, clear the answer box
+document.getElementById("clear").addEventListener("click",function() {
+  answerBox.innerHTML = "";
+});
+
+//if the equals button is clicked, evaluate the expression in the answer box
+document.getElementById("equals").addEventListener("click",function(){
+  answerBox.innerHTML = eval(answerBox.innerHTML);
+});
